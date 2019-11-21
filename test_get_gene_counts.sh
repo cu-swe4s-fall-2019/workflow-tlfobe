6c22df87f316a0a670adf9f790e2b16a26980d3f
tlfobe <lennyfobe@gmail.com>
2019-11-20 22:07:49 -0700

    added remote download for testing

--- test_get_gene_counts.sh
+++ test_get_gene_counts.sh
@@ -2,6 +2,9 @@
 test -e ssshtest ||  wget -q http://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
+# grab file for testing
+wget https://github.com/swe4s/lectures/raw/master/data_integration/gtex/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz
+
 run test_get_gene_counts python get_gene_counts.py --file_name GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz --gene_name SDHB --output_file out.txt
 assert_exit_code 0
 assert_no_stdout
@@ -20,4 +23,4 @@ run test_get_gene_counts_invalid_file python get_gene_counts.py --file_name GTEx
 assert_exit_code 1
 assert_in_stderr HAHA
 
-
+rm GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz
