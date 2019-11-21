473bfcd06f97a15afcf7886fcfbd41b6a436c9fa
tlfobe <lennyfobe@gmail.com>
2019-11-21 15:25:10 -0700

    updated pep8 style and travis.yml file

--- test_box.py
+++ test_box.py
@@ -5,6 +5,8 @@ import random
 
 gene_num = random.randint(0, 30)
 tissue_num = random.randint(0, 30)
+
+
 class TestGetTissueData(unittest.TestCase):
     def setUp(self):
         # tissues
@@ -15,7 +17,7 @@ class TestGetTissueData(unittest.TestCase):
         for i in range(gene_num):
             with open("DUMMY_GENE_"+str(i)+".txt", 'w') as f:
                 for j in range(random.randint(0, 500)):
-                    f.write(str(j)+" "+str(random.randint(1,10000))+"\n")
+                    f.write(str(j)+" "+str(random.randint(1, 10000))+"\n")
 
     def test_get_tissue_data_normal(self):
         genes = ["DUMMY_GENE_"+str(j) for j in range(gene_num)]
@@ -29,9 +31,10 @@ class TestGetTissueData(unittest.TestCase):
 
     def test_get_tissue_data_genes_type(self):
         self.assertRaises(TypeError, box.get_tissue_data, "tissue", "string!")
-    
+
     def test_get_tissue_data_genes_list_types(self):
-        self.assertRaises(TypeError, box.get_tissue_data, "tissue", ["string", 1, []])
+        self.assertRaises(TypeError, box.get_tissue_data,
+                          "tissue", ["string", 1, []])
 
     def tearDown(self):
         os.system("rm DUMMY*")
