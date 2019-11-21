859acd4603eee9f133488a4d4bf1f4e889b41ed4
tlfobe <lennyfobe@gmail.com>
2019-11-21 00:00:25 -0700

    added shells of funtions I will implement

---
+++ box.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import argparse
+matplotlib.use('Agg')
+
+plt.style.use('fivethirtyeight')
+
+def get_tissue_data(tissue, gene_list):
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
